360b31156dbf99887e29d9a89bfae56735df8003
Justo Soto <ju2484s@gmail.com>
2024-03-18 07:38:42 -0500

    handleResize

--- script/app.js
+++ script/app.js
@@ -1,41 +1,35 @@
 window.addEventListener("DOMContentLoaded", gameLoop);
-const animate = ["./photos/Mario.webp", "./photos/Bowser_1400x.webp"];
-let time = 0;
+let arjsComponent;
 
 function gameLoop() {
   let marker = document.querySelector("a-marker");
-  let sound = document.querySelector('[sound]');//document.querySelector('#sonido');
+  let sound = document.querySelector('[sound]');
   let scene = document.querySelector('a-scene');
-  // Obtener el componente arjs de la escena
-  let arjsComponent = scene.getAttribute('arjs');
-  arjsComponent= `${arjsComponent} displayWidth: ${Math.floor(window.innerWidth*0.5)}; displayHeight: ${Math.floor(window.innerHeight*0.5)};`
-  
-  function handleResize() {
-    // Obtener el ancho y el alto de la pantalla después de la rotación
-   // arjsComponent= `${arjsComponent} displayWidth: ${Math.floor(window.innerWidth*0.5)}; displayHeight: ${Math.floor(window.innerHeight*0.5)};`
-   console.log('handleResize')
 
+  function handleResize() {
+    // Verificar si la pantalla está en orientación apaisada
+    const isLandscape = window.innerWidth > window.innerHeight;
+
+    // Ajustar el componente arjs para coincidir con las nuevas dimensiones de la pantalla
+    arjsComponent = scene.getAttribute('arjs');
+    arjsComponent += ` displayWidth: ${isLandscape ? window.innerWidth : Math.floor(window.innerWidth * 0.5)};`;
+    arjsComponent += ` displayHeight: ${isLandscape ? window.innerHeight : Math.floor(window.innerHeight * 0.5)};`;
+    scene.setAttribute('arjs', arjsComponent);
   }
-  //window.addEventListener('resize', handleResize);
+
+  window.addEventListener('resize', handleResize);
+
   window.addEventListener("camera-init", function () {
-    // Muestra el contenido cuando se detecta el marcador
-    //console.log(sound.components.sound)
-   // arjsComponent.displayWidth = Math.floor(window.innerWidth*0.5);
-   // arjsComponent.displayHeight = Math.floor(window.innerHeight*0.5);
-   // arjsComponent= `${arjsComponent} displayWidth: ${Math.floor(window.innerWidth*0.5)}; displayHeight: ${Math.floor(window.innerHeight*0.5)};`
-   scene.setAttribute('arjs',arjsComponent);
-    console.log('camera-init:', scene)
-   
+    // Ajustar el tamaño de la pantalla inicialmente
+    handleResize();
   });
+
   marker.addEventListener("markerFound", function () {
-    // Muestra el contenido cuando se detecta el marcador
-    //console.log(sound.components.sound)
-    console.log('arjsComponent:', arjsComponent)
-    sound.components.sound.data.loop=true;
+    sound.components.sound.data.loop = true;
     sound.components.sound.playSound();
   });
+
   marker.addEventListener("markerLost", function () {
-    
     sound.components.sound.stopSound();
   });
 }
