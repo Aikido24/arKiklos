63da998b3a5c964434209d1a64f54e9e41340b94
Justo Soto <ju2484s@gmail.com>
2024-03-17 22:52:16 -0500

    handleResize

--- script/app.js
+++ script/app.js
@@ -1,36 +1,41 @@
+window.addEventListener("DOMContentLoaded", gameLoop);
+const animate = ["./photos/Mario.webp", "./photos/Bowser_1400x.webp"];
+let time = 0;
+
 function gameLoop() {
   let marker = document.querySelector("a-marker");
-  let sound = document.querySelector('[sound]');
+  let sound = document.querySelector('[sound]');//document.querySelector('#sonido');
   let scene = document.querySelector('a-scene');
+  // Obtener el componente arjs de la escena
   let arjsComponent = scene.getAttribute('arjs');
-  // Añade los valores iniciales de displayWidth y displayHeight
-  if (!arjsComponent.includes('displayWidth') && !arjsComponent.includes('displayHeight')) {
-    arjsComponent += ` displayWidth: ${Math.floor(window.innerWidth * 0.5)}; displayHeight: ${Math.floor(window.innerHeight * 0.5)};`;
-  }
-
+  arjsComponent= `${arjsComponent} displayWidth: ${Math.floor(window.innerWidth*0.5)}; displayHeight: ${Math.floor(window.innerHeight*0.5)};`
+  
   function handleResize() {
-    console.log('handleResize');
-    // Actualiza el tamaño del componente AR solo si no se ha actualizado antes
-    if (!arjsComponent.includes('displayWidth') && !arjsComponent.includes('displayHeight')) {
-      arjsComponent += ` displayWidth: ${Math.floor(window.innerWidth * 0.5)}; displayHeight: ${Math.floor(window.innerHeight * 0.5)};`;
-      scene.setAttribute('arjs', arjsComponent);
-    }
+    // Obtener el ancho y el alto de la pantalla después de la rotación
+   // arjsComponent= `${arjsComponent} displayWidth: ${Math.floor(window.innerWidth*0.5)}; displayHeight: ${Math.floor(window.innerHeight*0.5)};`
+   console.log('handleResize')
+
   }
-
   window.addEventListener('resize', handleResize);
-
   window.addEventListener("camera-init", function () {
-    scene.setAttribute('arjs', arjsComponent);
-    console.log('camera-init:', scene);
+    // Muestra el contenido cuando se detecta el marcador
+    //console.log(sound.components.sound)
+   // arjsComponent.displayWidth = Math.floor(window.innerWidth*0.5);
+   // arjsComponent.displayHeight = Math.floor(window.innerHeight*0.5);
+   // arjsComponent= `${arjsComponent} displayWidth: ${Math.floor(window.innerWidth*0.5)}; displayHeight: ${Math.floor(window.innerHeight*0.5)};`
+   scene.setAttribute('arjs',arjsComponent);
+    console.log('camera-init:', scene)
+   
   });
-
   marker.addEventListener("markerFound", function () {
-    console.log('arjsComponent:', arjsComponent);
-    sound.components.sound.data.loop = true;
+    // Muestra el contenido cuando se detecta el marcador
+    //console.log(sound.components.sound)
+    console.log('arjsComponent:', arjsComponent)
+    sound.components.sound.data.loop=true;
     sound.components.sound.playSound();
   });
-
   marker.addEventListener("markerLost", function () {
+    
     sound.components.sound.stopSound();
   });
 }
