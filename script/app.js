7bff45e6306d1b49e5878d938da9ee97780e0cfc
Justo Soto <ju2484s@gmail.com>
2024-03-17 19:53:55 -0500

    displayWidth and displayHeight

--- script/app.js
+++ script/app.js
@@ -5,10 +5,16 @@ let time = 0;
 function gameLoop() {
   let marker = document.querySelector("a-marker");
   let sound = document.querySelector('[sound]');//document.querySelector('#sonido');
+  let scene = document.querySelector('a-scene');
+  // Obtener el componente arjs de la escena
+  let arjsComponent = scene.getAttribute('arjs');
+  arjsComponent.displayWidth = window.innerWidth;
+  arjsComponent.displayHeight = window.innerHeight;
   
   marker.addEventListener("markerFound", function () {
     // Muestra el contenido cuando se detecta el marcador
-    console.log(sound.components.sound)
+    //console.log(sound.components.sound)
+    console.log('arjsComponent:',arjsComponent)
     sound.components.sound.data.loop=true;
     sound.components.sound.playSound();
   });
