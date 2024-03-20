15dc0992eaaa0c414e82e5eebe7d891094436c91
Justo Soto <ju2484s@gmail.com>
2024-03-20 09:08:25 -0500

    instruction

--- script/app.js
+++ script/app.js
@@ -6,7 +6,7 @@ function gameLoop() {
  
   let marker = document.querySelector("a-marker");
   let sound = document.querySelector('[sound]');
-  
+  let divOverlay = document.getElementById('div-overlay'); 
   
  
 
@@ -16,10 +16,12 @@ function gameLoop() {
   marker.addEventListener("markerFound", function () {
     sound.components.sound.data.loop = true;
     sound.components.sound.playSound();
+    divOverlay.style.visibility = 'hidden'
   });
 
   marker.addEventListener("markerLost", function () {
     sound.components.sound.stopSound();
+    divOverlay.style.visibility = 'visible'
   });
 }
 
