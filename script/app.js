cc4a35cdb878b42771c8f4099fcecb48ccd93f4a
Justo Soto <ju2484s@gmail.com>
2024-03-20 12:00:35 -0500

    botones

--- script/app.js
+++ script/app.js
@@ -7,8 +7,10 @@ function gameLoop() {
   let marker = document.querySelector("a-marker");
   let sound = document.querySelector('[sound]');
   let divOverlay = document.getElementById('div-overlay'); 
-  
- 
+  let whatsapp = document.getElementById('whatsapp'); 
+  let instagram = document.getElementById('instagram'); 
+  let pdf = document.getElementById('pdf'); 
+  let webPage = document.getElementById('webPage'); 
 
   
 
@@ -17,11 +19,19 @@ function gameLoop() {
     //sound.components.sound.data.loop = true;
     //sound.components.sound.playSound();
     divOverlay.style.visibility = 'hidden'
+    whatsapp.style.visibility = 'visible'
+    instagram.style.visibility = 'visible'
+    pdf.style.visibility = 'visible'
+    webPage.style.visibility = 'visible'
   });
 
   marker.addEventListener("markerLost", function () {
    // sound.components.sound.stopSound();
     divOverlay.style.visibility = 'visible'
+    whatsapp.style.visibility = 'hidden'
+    instagram.style.visibility = 'hidden'
+    pdf.style.visibility = 'hidden'
+    webPage.style.visibility = 'hidden'
   });
 }
 
