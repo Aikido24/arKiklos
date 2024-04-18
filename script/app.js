1fa814374342e033c67b8f433bc7f352b654d28b
Justo Soto <ju2484s@gmail.com>
2024-04-18 14:47:13 -0500

    sapi

--- script/app.js
+++ script/app.js
@@ -2,39 +2,38 @@ window.addEventListener("DOMContentLoaded", gameLoop);
 let arjsComponent;
 
 function gameLoop() {
-  
- 
   let marker = document.querySelector("a-marker");
-  let sound = document.querySelector('[sound]');
-  let divOverlay = document.getElementById('div-overlay'); 
-  let whatsapp = document.getElementById('whatsapp'); 
-  let instagram = document.getElementById('instagram'); 
-  let bot = document.getElementById('bot'); 
-  let webPage = document.getElementById('webPage'); 
-  whatsapp.style.visibility = 'hidden'
-  instagram.style.visibility = 'hidden'
-  bot.style.visibility = 'hidden'
-  webPage.style.visibility = 'hidden'
-  
-  
+  let sound = document.querySelector("[sound]");
+  let divOverlay = document.getElementById("div-overlay");
+  let whatsapp = document.getElementById("whatsapp");
+  let instagram = document.getElementById("instagram");
+  let bot = document.getElementById("bot");
+  let webPage = document.getElementById("webPage");
+  let webPage1 = document.getElementById("sape");
+  whatsapp.style.visibility = "hidden";
+  instagram.style.visibility = "hidden";
+  bot.style.visibility = "hidden";
+  webPage.style.visibility = "visible";
+  webPage1.style.visibility = "visible";
 
   marker.addEventListener("markerFound", function () {
     //sound.components.sound.data.loop = true;
     //sound.components.sound.playSound();
-    divOverlay.style.visibility = 'hidden'
-    whatsapp.style.visibility = 'visible'
-    instagram.style.visibility = 'visible'
-    bot.style.visibility = 'visible'
-    webPage.style.visibility = 'visible'
+    divOverlay.style.visibility = "hidden";
+    whatsapp.style.visibility = "visible";
+    instagram.style.visibility = "visible";
+    bot.style.visibility = "visible";
+    webPage.style.visibility = "visible";
+    webPage1.style.visibility = "visible";
   });
 
   marker.addEventListener("markerLost", function () {
-   // sound.components.sound.stopSound();
-    divOverlay.style.visibility = 'visible'
-    whatsapp.style.visibility = 'hidden'
-    instagram.style.visibility = 'hidden'
-    bot.style.visibility = 'hidden'
-    webPage.style.visibility = 'hidden'
+    // sound.components.sound.stopSound();
+    divOverlay.style.visibility = "visible";
+    whatsapp.style.visibility = "hidden";
+    instagram.style.visibility = "hidden";
+    bot.style.visibility = "hidden";
+    webPage.style.visibility = "hidden";
+    webPage1.style.visibility = "hidden";
   });
 }
-
